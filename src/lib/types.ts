c7d16965182ce883af5cd4821e8610d7d9cfab3b
有村ロミ <biyuehuya@gmail.com>
2025-12-29 01:31:11 +0800

    feat: update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,7 @@
 export interface Word {
   id: number
   word: string
-  type: 'n.' | 'v.' | 'adj.' | 'adv.' | 'pron.' | 'conj.' | 'intj.' | 'prep.'
+  type: 'n.' | 'v.' | 'adj.' | 'adv.' | 'pron.' | 'conj.' | 'interj.' | 'prep.' |   ‘aux.' | 'num. | 'part.'
   meaning: string
   example: string
 }
